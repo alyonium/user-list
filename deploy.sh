2a2936c46c281264f76284922d75e59a41b3da0f
alyonium <alyonium2.0@gmail.com>
2021-01-05 13:26:30 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+yarn build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:alyonium/user-list.git master:gh-pages
+
+cd -
