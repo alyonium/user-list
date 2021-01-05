6fadc7df1aba0c00de7a35e020f943cac3b881b9
alyonium <alyonium2.0@gmail.com>
2021-01-05 13:37:37 +0300

    deploy

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
-Vue.prototype.$httpRequest = 'http://localhost:3000';
+Vue.prototype.$httpRequest = 'https://user-list-123.herokuapp.com';
 
 new Vue({
   vuetify,
